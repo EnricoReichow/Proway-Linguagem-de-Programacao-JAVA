c7299c6e147738432542e92f8450113ee61adee8
Enrico Bernz Reichow Santos <71547@PROWAY.TREINA>
2022-02-23 22:07:41 -0300

    aula 4 finished

---
+++ ProjectMVC/src/br/com/projectMVC/DAO/GenericDAO.java
@@ -0,0 +1,17 @@
+package br.com.projectMVC.DAO;
+
+import java.util.List;
+
+public interface GenericDAO {
+	
+	public List<Object> getAll();
+	
+	public Object getById (int id);
+	
+	public Boolean insert (Object object);  //this interface can be used for all the projects that use CRUD
+	
+	public Boolean update(Object object);
+	
+	public void delete(int id);
+	
+}
